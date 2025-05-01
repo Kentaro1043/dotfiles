5337e356002be6382961e9675080a8dfd154468b
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-05-01 18:55:07 +0900

    🔥 コメントを削除

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,5 +1,3 @@
-# This is your home-manager configuration file
-# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 {
   inputs,
   outputs,
@@ -8,15 +6,7 @@
   pkgs,
   ...
 }: {
-  # Import other home-manager modules here
   imports = [
-    # If you want to use modules your own flake exports (from modules/home-manager):
-    # outputs.homeManagerModules.example
-
-    # Or modules exported from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModules.default
-
-    # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
   ];
 
@@ -26,19 +16,8 @@
       outputs.overlays.additions
       outputs.overlays.modifications
       outputs.overlays.unstable-packages
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
     
-    # Configure nixpkgs instance
     config = {
       allowUnfree = true;
     };
@@ -49,8 +28,7 @@
     homeDirectory = if pkgs.stdenv.isDarwin then "/Users/kentaro" else "/home/kentaro";
   };
 
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
+
   home.packages = with pkgs; [ 
     hello
 
@@ -70,6 +48,7 @@
     kustomize-sops
   ];
 
+
   home.file = {
     ".gitconfig" = {
       text = ''
@@ -80,11 +59,10 @@
     };
   };
 
-  # Enable home-manager and git
+
   programs.home-manager.enable = true;
   programs.git.enable = true;
 
-  # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion

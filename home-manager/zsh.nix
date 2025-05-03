33ff6184d2ed7f538b8f5dce759d693f19a08317
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-05-03 21:24:47 +0900

    ✨ agnoster theme

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -9,21 +9,29 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    autosuggestion.enable = true;
+    autosuggestion = {
+      enable = true;
+      highlight = "fg=yellow";
+    };
     syntaxHighlighting.enable = true;
 
     oh-my-zsh = {
       enable = true;
-      theme = "robbyrussell";
-    };
-
-    shellAliases = {
-      ll = "ls -l";
+      theme = "agnoster";
     };
 
     initExtra = ''
       # Enable keychain
       eval `keychain --eval --agents ssh ~/.ssh/id_ed25519`
+    '';
+
+    envExtra = ''
+      # Disable user@hostname for agnoster theme
+      export DEFAULT_USER="kentaro"
+
+      # Enable Volta
+      export VOLTA_HOME="$HOME/.volta"
+      export PATH="$VOLTA_HOME/bin:$PATH"
     '';
   };
 

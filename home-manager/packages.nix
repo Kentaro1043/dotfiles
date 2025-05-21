a3c5f909739e521bdc9b0a795694888a9c2e34eb
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-05-22 01:03:21 +0900

    ✨ marp用にフォント追加

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -18,8 +18,9 @@
       gh
 
       # fonts
-      #noto-fonts-cjk-sans
-      #noto-fonts-cjk-serif
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
       #source-han-sans
       #source-han-sans-vf-otf
       #source-han-sans-vf-ttf

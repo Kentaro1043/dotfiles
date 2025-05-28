93ae6bb6080070eb80a366a162025b9d88650ca2
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-05-28 15:08:51 +0900

    ✨ ssh先

--- home-manager/ssh.nix
+++ home-manager/ssh.nix
@@ -20,6 +20,26 @@
         user = "git";
         identityFile = "~/.ssh/id_ed25519";
       };
+      "csc301.tokyotech.org" = {
+        user = "kentaro1043";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+      "c1-203.tokyotech.org" = {
+        user = "kentaro1043";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+      "ict201.tokyotech.org" = {
+        user = "kentaro1043";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+      "ict202.tokyotech.org" = {
+        user = "kentaro1043";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+      "ict203.tokyotech.org" = {
+        user = "kentaro1043";
+        identityFile = "~/.ssh/id_ed25519";
+      };
     };
   };
 }

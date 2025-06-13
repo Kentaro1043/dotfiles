244609b168de3d460b564b2928cbf41de940790e
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-06-13 13:06:07 +0900

    pull.rebase config

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -11,5 +11,8 @@
     userName = "Kentaro1043";
     userEmail = "71170923+Kentaro1043@users.noreply.github.com";
     lfs.enable = true;
+    extraConfig = {
+      pull.rebase = false;
+    };
   };
 }

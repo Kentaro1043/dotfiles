fd78975f349b21fe169089d5246f31737b994182
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-07-25 05:16:13 +0900

    starship

--- home-manager/programs/starship.nix
+++ home-manager/programs/starship.nix
@@ -6,14 +6,47 @@
 }: {
   programs.starship = {
     enable = true;
-    settings = {
-      sudo.disabled = false;
-      kubernetes = {
-        disabled = false;
-        detect_files = [
-          "kustomization.yaml"
-        ];
+
+    # 何故か機能しない
+    #presets = [
+    #  "bracketed-segments"
+    #];
+
+    # Downloaded preset from https://starship.rs/ja-JP/presets/bracketed-segments
+    settings =
+      builtins.fromTOML (builtins.readFile ./bracketed-segments.toml)
+      // {
+        sudo.disabled = false;
+
+        kubernetes = {
+          disabled = false;
+          detect_files = [
+            "kustomization.yaml"
+          ];
+        };
+
+        time = {
+          disabled = false;
+          format = "[\\[🕙 $time\\]]($style) ";
+          time_format = "%R";
+        };
+
+        # Dracula
+        # https://draculatheme.com/starship
+        aws.style = "bold #ffb86c";
+        cmd_duration.style = "bold #f1fa8c";
+        directory.style = "bold #50fa7b";
+        hostname.style = "bold #ff5555";
+        git_branch.style = "bold #ff79c6";
+        git_status.style = "bold #ff5555";
+        username = {
+          format = "[$user]($style) on ";
+          style_user = "bold #bd93f9";
+        };
+        character = {
+          success_symbol = "[λ](bold #f8f8f2)";
+          error_symbol = "[λ](bold #ff5555)";
+        };
       };
-    };
   };
 }

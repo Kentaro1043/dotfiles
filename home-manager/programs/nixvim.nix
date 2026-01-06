a4fdc5d15bb8b6431887bd8b734ece38941a09a3
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-01-07 02:55:03 +0900

    nvimプラグイン

--- home-manager/programs/nixvim.nix
+++ home-manager/programs/nixvim.nix
@@ -1,5 +1,31 @@
 {lib, ...}: {
   programs.nixvim = {
     enable = true;
+
+    plugins = {
+      # UI
+      noice.enable = true;
+      lualine.enable = true;
+      bufferline.enable = true;
+      neo-tree.enable = true;
+      web-devicons.enable = true;
+
+      # Development
+      lsp = {
+        enable = true;
+        servers.nil_ls.enable = true; # Nix用LSP
+      };
+      treesitter.enable = true;
+      telescope.enable = true;
+
+      # Assistants
+      which-key.enable = true;
+      gitsigns.enable = true;
+    };
+
+    colorschemes.dracula = {
+      enable = true;
+      autoLoad = true;
+    };
   };
 }

61fe9c9349c1431c8223f1e6fc0f0b4757371815
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-02-01 00:39:19 +0900

    Enable zsh

--- nixos/kentaro-desktop/configuration.nix
+++ nixos/kentaro-desktop/configuration.nix
@@ -93,6 +93,8 @@
   # Install firefox.
   programs.firefox.enable = true;
 
+  programs.zsh.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [

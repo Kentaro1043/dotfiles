bb413877a0baa7de2340e970b3f863ce7f241502
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-02-01 01:34:02 +0900

    Add font

--- nixos/kentaro-desktop/configuration.nix
+++ nixos/kentaro-desktop/configuration.nix
@@ -66,6 +66,35 @@
     #media-session.enable = true;
   };
 
+  # Fonts
+  fonts = {
+    fonts = with pkgs; [
+      noto-fonts-cjk-serif
+      noto-fonts-cjk-sans
+      migu
+      noto-fonts
+      noto-fonts-monochrome-emoji
+    ];
+    fontDir.enable = true;
+    fontconfig = {
+      defaultFonts = {
+        serif = [
+          "Noto Serif CJK JP"
+          "Noto Emoji"
+        ];
+        sansSerif = [
+          "Noto Sans CJK JP"
+          "Noto Emoji"
+        ];
+        monospace = [
+          "Noto Sans Mono"
+          "Noto Emoji"
+        ];
+        emoji = ["Noto Emoji"];
+      };
+    };
+  };
+
   # User
   users.users.kentaro = {
     isNormalUser = true;

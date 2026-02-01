9128c20e12c3b7a690575a4d487992cef95be5ac
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-02-02 03:18:04 +0900

    Add preload models

--- nixos/kentaro-desktop/services/ollama.nix
+++ nixos/kentaro-desktop/services/ollama.nix
@@ -5,5 +5,21 @@
     environmentVariables = {
       LD_LIBRARY_PATH = "${pkgs.linuxPackages.nvidia_x11}/lib:$LD_LIBRARY_PATH";
     };
+
+    loadModels = [
+      # OpenAI
+      "gpt-oss:20b-cloud"
+      "gpt-oss:120b-cloud"
+
+      # Roo Code
+      "qwen3-coder:480b-cloud"
+      "deepseek-v3.1:671b-cloud"
+
+      # Continue
+      "llama3.1:8b"
+      "qwen2.5-coder:7b"
+      "qwen2.5-coder:1.5b-base"
+      "nomic-embed-text:latest"
+    ];
   };
 }

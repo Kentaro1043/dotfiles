4fd2cff1413f3eb98eb62c94e53fdff8ac450c72
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-02-02 13:44:30 +0900

    Add opencode config

--- home-manager/programs/opencode.nix
+++ home-manager/programs/opencode.nix
@@ -4,6 +4,25 @@
     package = pkgs.unstable.opencode;
     settings = {
       theme = "dracula";
+
+      # Model
+      provider = {
+        ollama = {
+          npm = "@ai-sdk/openai-compatible";
+          name = "Ollama";
+          options = {
+            baseURL = "http://localhost:11434/v1";
+          };
+          models = {
+            "qwen3-coder:480b-cloud" = {
+              name = "qwen3-coder:480b-cloud";
+            };
+            "deepseek-v3.1:671b-cloud" = {
+              name = "deepseek-v3.1:671b-cloud";
+            };
+          };
+        };
+      };
     };
     themes = {
       dracula = ./opencode-dracula.json;

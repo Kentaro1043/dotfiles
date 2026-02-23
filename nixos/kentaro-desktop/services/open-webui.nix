bf35a7334d0fc30acfe5ec7f1484047bd573a3f5
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-02-24 05:18:28 +0900

    Add Web Search Config

--- nixos/kentaro-desktop/services/open-webui.nix
+++ nixos/kentaro-desktop/services/open-webui.nix
@@ -10,6 +10,14 @@
       # Auth
       WEBUI_AUTH = "False";
 
+      # Web Search
+      ENABLE_WEB_SEARCH = "True";
+      ENABLE_SEARCH_QUERY_GENERATION = "True";
+      WEB_SEARCH_RESULT_COUNT = "3";
+      WEB_SEARCH_CONCURRENT_REQUESTS = "0"; # Unlimit
+      WEB_LOADER_CONCURRENT_REQUESTS = "10";
+      WEB_SEARCH_ENGINE = "duckduckgo";
+
       # Telemetry
       ANONYMIZED_TELEMETRY = "False";
       DO_NOT_TRACK = "True";

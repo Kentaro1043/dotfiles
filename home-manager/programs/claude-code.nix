50ba7198611f29fd4bbaae39a9b0afdfa4a05f01
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-02-26 10:28:11 +0900

    Add claude-code

---
+++ home-manager/programs/claude-code.nix
@@ -0,0 +1,70 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  programs.claude-code = {
+    enable = true;
+    package = pkgs.unstable.claude-code;
+    mcpServers = {
+      context7 = {
+        command = "npx";
+        args = [
+          "-y"
+          "@upstash/context7-mcp@latest"
+        ];
+      };
+      filesystem = {
+        command = "npx";
+        args =
+          [
+            "-y"
+            "@modelcontextprotocol/server-filesystem"
+          ]
+          ++ lib.optionals pkgs.stdenv.isDarwin [
+            "/Users/kentaro/source/repos"
+          ]
+          ++ lib.optionals pkgs.stdenv.isLinux [
+            "/home/kentaro/source/repos"
+          ];
+      };
+      fetch = {
+        command = "uvx";
+        args = [
+          "mcp-server-fetch"
+        ];
+      };
+      ddg-search = {
+        command = "uvx";
+        args = [
+          "duckduckgo-mcp-server"
+        ];
+      };
+      sequential-thinking = {
+        command = "npx";
+        args = [
+          "-y"
+          "@modelcontextprotocol/server-sequential-thinking"
+        ];
+      };
+      serena = {
+        command = "uvx";
+        args = [
+          "--from"
+          "git+https://github.com/oraios/serena"
+          "serena"
+          "start-mcp-server"
+          "--context"
+          "ide"
+          "--project-from-cwd"
+        ];
+      };
+      codex = {
+        command = "codex";
+        args = [
+          "mcp-server"
+        ];
+      };
+    };
+  };
+}

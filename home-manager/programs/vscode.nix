5938988cad30e623e14f369ce2edd5bce1814225
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-03-27 15:42:10 +0900

    Add biome extension

--- home-manager/programs/vscode.nix
+++ home-manager/programs/vscode.nix
@@ -63,6 +63,8 @@
         astro-build.astro-vscode
         # toml
         tamasfe.even-better-toml
+        # JS/TS
+        biomejs.biome
       ];
       enableExtensionUpdateCheck = false;
       enableMcpIntegration = true;

3d8152cb5e15ba98daa773ffbfb8a253679b70af
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-04-08 22:53:56 +0900

    Fix kind

--- nixos/kentaro-desktop/configuration.nix
+++ nixos/kentaro-desktop/configuration.nix
@@ -42,13 +42,16 @@
     hostName = "kentaro-desktop";
     networkmanager.enable = true;
 
-    firewall.allowedUDPPortRanges = [
-      # KDE Connect
-      {
-        from = 1714;
-        to = 1764;
-      }
-    ];
+    firewall = {
+      checkReversePath = false;
+      allowedUDPPortRanges = [
+        # KDE Connect
+        {
+          from = 1714;
+          to = 1764;
+        }
+      ];
+    };
   };
   services.avahi = {
     enable = true;

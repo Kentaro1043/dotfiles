56707612b7e1846cc14466c5a363ff42d3c727f5
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-04-29 16:44:46 +0900

    Add trusted users

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -4,6 +4,7 @@
   determinateNix.customSettings = {
     # extra-experimental-features = [ "external-builders" ];
     # external-builders = ''[{"systems":["aarch64-linux","x86_64-linux"],"program":"/usr/local/bin/determinate-nixd","args":["builder"]}]'';
+    trusted-users = ["root" "kentaro"];
   };
 
   system = {

30b8a30b65a2848623c02975f0de571f6390be27
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-06-12 01:40:50 +0900

    Add textlint extension

--- home-manager/programs/vscode.nix
+++ home-manager/programs/vscode.nix
@@ -21,76 +21,80 @@
     profiles.default = {
       userSettings = builtins.fromJSON (builtins.readFile ./vscode-settings.json);
       keybindings = builtins.fromJSON (builtins.readFile ./vscode-keybindings.json);
-      extensions = with pkgs.nix-vscode-extensions.open-vsx; [
-        # Editor
-        dracula-theme.theme-dracula
-        pkief.material-icon-theme
-        eamodio.gitlens
-        vivaxy.vscode-conventional-commits
-        jetify.devbox
-        esbenp.prettier-vscode
+      extensions = with pkgs.nix-vscode-extensions.open-vsx-release;
+        [
+          # Editor
+          dracula-theme.theme-dracula
+          pkief.material-icon-theme
+          eamodio.gitlens
+          vivaxy.vscode-conventional-commits
+          jetify.devbox
+          esbenp.prettier-vscode
 
-        # AI
-        continue.continue
-        saoudrizwan.claude-dev # Cline
-        google.gemini-cli-vscode-ide-companion
-        qwenlm.qwen-code-vscode-ide-companion
-        sst-dev.opencode
-        anthropic.claude-code # Unfree
-        # openai.chatgpt # Unfree
+          # AI
+          continue.continue
+          saoudrizwan.claude-dev # Cline
+          google.gemini-cli-vscode-ide-companion
+          qwenlm.qwen-code-vscode-ide-companion
+          sst-dev.opencode
+          anthropic.claude-code # Unfree
+          # openai.chatgpt # Unfree
 
-        # Platform
-        ms-kubernetes-tools.vscode-kubernetes-tools
+          # Platform
+          ms-kubernetes-tools.vscode-kubernetes-tools
 
-        # Languages
-        # Nix
-        jnoortheen.nix-ide
-        # C/C++
-        llvm-vs-code-extensions.vscode-clangd
-        llvm-vs-code-extensions.lldb-dap
-        vadimcn.vscode-lldb
-        # yaml
-        redhat.vscode-yaml
-        redhat.ansible
-        # Typst
-        myriad-dreamin.tinymist
-        # Go
-        golang.go
-        # Python
-        ms-python.python
-        ms-python.debugpy
-        ms-python.vscode-python-envs
-        astral-sh.ty
-        charliermarsh.ruff
-        # Jupyter
-        ms-toolsai.jupyter
-        ms-toolsai.jupyter-keymap
-        google.colab
-        # Astro
-        astro-build.astro-vscode
-        # toml
-        tamasfe.even-better-toml
-        # JS/TS
-        biomejs.biome
-        # Vue.js
-        vue.volar
-        # Java
-        vscjava.vscode-java-debug
-        vscjava.vscode-java-test
-        vscjava.vscode-maven
-        vscjava.vscode-gradle
-        vscjava.vscode-java-dependency
-        redhat.java
-        # Docker
-        docker.docker
-        exiasr.hadolint
-        # Terraform
-        opentofu.vscode-opentofu
-        # GitHub Actions
-        github.vscode-github-actions
-        # Markdown
-        davidanson.vscode-markdownlint
-      ];
+          # Languages
+          # Nix
+          jnoortheen.nix-ide
+          # C/C++
+          llvm-vs-code-extensions.vscode-clangd
+          llvm-vs-code-extensions.lldb-dap
+          vadimcn.vscode-lldb
+          # yaml
+          redhat.vscode-yaml
+          redhat.ansible
+          # Typst
+          myriad-dreamin.tinymist
+          # Go
+          golang.go
+          # Python
+          ms-python.python
+          ms-python.debugpy
+          ms-python.vscode-python-envs
+          astral-sh.ty
+          charliermarsh.ruff
+          # Jupyter
+          ms-toolsai.jupyter
+          ms-toolsai.jupyter-keymap
+          google.colab
+          # Astro
+          astro-build.astro-vscode
+          # toml
+          tamasfe.even-better-toml
+          # JS/TS
+          biomejs.biome
+          # Vue.js
+          vue.volar
+          # Java
+          vscjava.vscode-java-debug
+          vscjava.vscode-java-test
+          vscjava.vscode-maven
+          vscjava.vscode-gradle
+          vscjava.vscode-java-dependency
+          redhat.java
+          # Docker
+          docker.docker
+          exiasr.hadolint
+          # Terraform
+          opentofu.vscode-opentofu
+          # GitHub Actions
+          github.vscode-github-actions
+          # Markdown
+          davidanson.vscode-markdownlint
+        ]
+        ++ [
+          pkgs.nix-vscode-extensions.vscode-marketplace-release."3w36zj6".textlint
+        ];
       enableExtensionUpdateCheck = false;
       enableMcpIntegration = true;
     };

85ca7b6933c897b6f0481d20f2e74f0924fe42a1
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-07-13 16:18:46 +0900

    Change directory structure

--- nixos/kentaro-desktop/storage.nix
+++ nixos/kentaro-desktop/storage.nix
@@ -1,13 +1,23 @@
 {
   fileSystems."/mnt/hdd" = {
-    device = "/dev/disk/by-uuid/79FC-F92C";
-    fsType = "exfat";
+    device = "/dev/disk/by-label/Linux";
+    fsType = "ext4";
+    options = [
+      "nofail"
+      "x-systemd.device-timeout=5s"
+    ];
+  };
+
+  fileSystems."/mnt/shared" = {
+    device = "/dev/disk/by-label/Shared";
+    fsType = "ntfs3";
     options = [
       "nofail"
       "x-systemd.device-timeout=5s"
       "uid=1000"
       "gid=100"
       "umask=0022"
+      "windows_names"
     ];
   };
 }

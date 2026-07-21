303f016d6a01ac812135daae8ead88328b8d80b6
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-07-21 20:56:48 +0900

    スリープ無効化

--- nixos/kentaro-desktop/configuration.nix
+++ nixos/kentaro-desktop/configuration.nix
@@ -162,8 +162,12 @@ in {
     videoDrivers = ["amdgpu"];
   };
 
-  # Enable the GNOME Desktop Environment.
+  # Enable the KDE Plasma Desktop Environment.
   services.desktopManager.plasma6.enable = true;
+  environment.etc."xdg/powerdevilrc".text = ''
+    [AC][SuspendAndShutdown]
+    AutoSuspendAction[$i]=0
+  '';
   services.displayManager.sddm = {
     enable = true;
 

86249708ea876a862b4a24ba7e3cf79196c51f08
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-07-26 11:19:57 +0900

    fix: 🐛 Numtideのバイナリキャッシュを信頼

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -15,6 +15,12 @@ in {
   determinateNix.customSettings = {
     # extra-experimental-features = [ "external-builders" ];
     # external-builders = ''[{"systems":["aarch64-linux","x86_64-linux"],"program":"/usr/local/bin/determinate-nixd","args":["builder"]}]'';
+    extra-substituters = [
+      "https://cache.numtide.com"
+    ];
+    extra-trusted-public-keys = [
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+    ];
     trusted-users = ["root" "kentaro"];
   };
 

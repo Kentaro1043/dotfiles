039254839fa9ed200607ea96e98ae9d104d432fb
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-07-31 15:31:32 +0900

    refactor: ♻️ OpenRazerの一時処理を削除

--- home-manager/nixos/openrazer.nix
+++ home-manager/nixos/openrazer.nix
@@ -1,17 +1,6 @@
-{
-  lib,
-  pkgs,
-  ...
-}: {
-  xdg.configFile."openrazer/razer.conf" = {
-    force = true;
-    text = ''
-      [Startup]
-      battery_notifier = False
-    '';
-  };
-
-  home.activation.restartOpenRazerDaemon = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    run ${lib.getExe' pkgs.systemd "systemctl"} --user try-restart openrazer-daemon.service
+{...}: {
+  xdg.configFile."openrazer/razer.conf".text = ''
+    [Startup]
+    battery_notifier = False
   '';
 }

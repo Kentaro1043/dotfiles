84165016e4a6e43a0da60297f145c246191abb7e
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-07-31 15:34:42 +0900

    fix: 🐛 Plasma再読み込み時のPATH破損を修正

--- home-manager/nixos/activation.nix
+++ home-manager/nixos/activation.nix
@@ -4,9 +4,7 @@
   ...
 }:
 lib.mkIf pkgs.stdenv.isLinux {
-  home.activation.refreshPlasmaApplicationMenu = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    run ${lib.getExe' pkgs.dbus "dbus-update-activation-environment"} --systemd XDG_DATA_DIRS PATH
+  home.activation.refreshPlasmaApplicationMenu = lib.hm.dag.entryAfter ["linkGeneration"] ''
     run ${lib.getExe' pkgs.kdePackages.kservice "kbuildsycoca6"} --noincremental
-    run ${lib.getExe' pkgs.systemd "systemctl"} --user restart plasma-plasmashell.service
   '';
 }

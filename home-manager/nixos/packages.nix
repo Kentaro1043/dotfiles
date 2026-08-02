6cc50f443e51fb12a308e398257d16f6c607897a
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-08-02 21:59:44 +0900

    fix: 🐛 PyCharmをXWaylandで起動

--- home-manager/nixos/packages.nix
+++ home-manager/nixos/packages.nix
@@ -2,42 +2,51 @@
   pkgs,
   lib,
   ...
-}:
-lib.mkIf pkgs.stdenv.isLinux
-{
-  home.packages = with pkgs; [
-    # GUI apps
-    discord
-    ghostty
-    unstable.jetbrains.pycharm
-    rpi-imager
-    libreoffice
-    freelens-bin
-    containerlab
-    devpod-desktop
-    mixxx
-    qtcreator
-    sidequest
-    dbeaver-bin
-    slack
-    vlc
-    woeusb-ng
-    zen-browser-bin
-    ardour
-    zrythm
-    surge-XT
-    usbutils
-    wineWow64Packages.stable
-    winetricks
-    warp-terminal
-    godot
-    # rustdesk
-    mission-center
-    feishin
-    obs-studio
-    shotcut
-    hermes-desktop
-    x42-avldrums
-    chromium
-  ];
-}
+}: let
+  pycharmXWayland = pkgs.symlinkJoin {
+    name = "pycharm-xwayland";
+    paths = [pkgs.unstable.jetbrains.pycharm];
+    nativeBuildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      wrapProgram "$out/bin/pycharm" \
+        --add-flags "-Dawt.toolkit.name=XToolkit"
+    '';
+  };
+in
+  lib.mkIf pkgs.stdenv.isLinux {
+    home.packages = with pkgs; [
+      # GUI apps
+      discord
+      ghostty
+      pycharmXWayland
+      rpi-imager
+      libreoffice
+      freelens-bin
+      containerlab
+      devpod-desktop
+      mixxx
+      qtcreator
+      sidequest
+      dbeaver-bin
+      slack
+      vlc
+      woeusb-ng
+      zen-browser-bin
+      ardour
+      zrythm
+      surge-XT
+      usbutils
+      wineWow64Packages.stable
+      winetricks
+      warp-terminal
+      godot
+      # rustdesk
+      mission-center
+      feishin
+      obs-studio
+      shotcut
+      hermes-desktop
+      x42-avldrums
+      chromium
+    ];
+  }
